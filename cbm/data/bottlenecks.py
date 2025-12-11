ffcc0f91ff34e183c5956ff92173a9eba32a17af
simonbing <bings@student.ethz.ch>
2025-12-11 15:14:17 +0100

    Updates MLP sampling for bottleneck and mechanism functions

--- cbm/data/bottlenecks.py
+++ cbm/data/bottlenecks.py
@@ -36,12 +36,75 @@ def sample_lin_bottleneck(rs, d_micro, d_bottleneck):
 
 
 def sample_nonlin_bottleneck(rs, d_micro, d_bottleneck):
-    hidden_layers = 4
-    nonlin = 'leaky_relu'
+    hidden_layers = 2
+    # nonlin = 'relu'
+    # nonlin = 'leaky_relu'
+    nonlin = 'sigmoid'
+    # nonlin = 'none'
 
     return sample_mlp(rs=rs, in_dim=d_micro, out_dim=d_bottleneck,
                       hidden_dim=d_micro, hidden_layers=hidden_layers,
                       nonlinearity=nonlin)
+
+    # w0 = rs.uniform(size=(d_micro, d_bottleneck))
+    # # Make sure rank is correct
+    # while np.linalg.matrix_rank(w0) < d_bottleneck:
+    #     w0 = rs.uniform(size=(d_micro, d_bottleneck))
+
+    # # See if QR decomposition leads to better behaved function
+    # # Q, R = np.linalg.qr(w0)
+    # # w0 = Q
+
+    # # w0 = np.asarray([[1, 0, 0, 0], [0, 1, 0, 0]]).T
+
+    # w1 = rs.uniform(size=(d_micro, d_micro))
+    # # Make sure rank is correct
+    # while np.linalg.matrix_rank(w1) < d_micro:
+    #     w1 = rs.uniform(size=(d_micro, d_micro))
+
+    # w2 = rs.uniform(size=(d_micro, d_micro))
+    # # Make sure rank is correct
+    # while np.linalg.matrix_rank(w2) < d_micro:
+    #     w2 = rs.uniform(size=(d_micro, d_micro))
+
+    # w3 = rs.uniform(size=(d_micro, d_micro))
+    # # Make sure rank is correct
+    # while np.linalg.matrix_rank(w3) < d_micro:
+    #     w3 = rs.uniform(size=(d_micro, d_micro))
+
+    # w4 = rs.uniform(size=(d_micro, d_micro))
+    # # Make sure rank is correct
+    # while np.linalg.matrix_rank(w4) < d_micro:
+    #     w4 = rs.uniform(size=(d_micro, d_micro))
+
+    # # Q, R = np.linalg.qr(w1)
+    # # w1 = Q
+
+    # # Q, R = np.linalg.qr(w2)
+    # # w2 = Q
+
+    # # Q, R = np.linalg.qr(w3)
+    # # w3 = Q
+
+    # # Q, R = np.linalg.qr(w4)
+    # # w4 = Q
+
+    # w = w1 @ w2 @ w3 @ w4 @ w0
+
+    # def leaky_relu(x):
+    #     if x < 0:
+    #         return 0.1 * x
+    #     else:
+    #         return x
+
+    # def sigmoid(x):
+    #     return 1 / (1 + np.exp(-x))
+
+    # def f(x):
+    #     # return np.vectorize(leaky_relu)(x @ w)
+    #     return x @ w
+
+    # return f
 
 
 def manual_nonlinear(rs, d_micro, d_bottleneck):
