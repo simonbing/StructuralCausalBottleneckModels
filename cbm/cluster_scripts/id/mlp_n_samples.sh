592588a0d99106f7a5ff69316807de25855c3b99
simonbing <bings@student.ethz.ch>
2026-01-06 11:29:11 +0100

    Cluster script

--- cbm/cluster_scripts/id/mlp_n_samples.sh
+++ cbm/cluster_scripts/id/mlp_n_samples.sh
@@ -2,4 +2,4 @@
 
 sbatch ../cluster_gpu.sh python ../../identifiability_experiment.py \
 --x n_samples --x_values 1000 --estimation_mode mlp \
---results_root /work/bd1083/b382081/projects/CausalBottleneckModels/results
+--results_root /work/bd1083/b382081/projects/CausalBottleneckModels/results_new
