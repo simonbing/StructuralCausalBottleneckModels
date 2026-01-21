c9d18a13a3d2f7eb819dffab74e5a21dab99dfad
simonbing <bings@student.ethz.ch>
2026-01-21 14:28:28 +0100

    cluster script

--- cbm/cluster_scripts/tf/mlp_tf.sh
+++ cbm/cluster_scripts/tf/mlp_tf.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
 sbatch ../cluster_gpu.sh python ../../transfer_experiment.py \
---seed 0 --d_micro 10 --train_sample_sizes 10,20,50,500,1000 --n_bn_train 50000 --mode nonlinear \
+--seed 0 --d_micro 10 --train_sample_sizes 200,500,1000,2000,10000 --n_bn_train 50000 --mode nonlinear \
 --results_root /work/bd1083/b382081/projects/CausalBottleneckModels/results_2layer_swish_deep_mlp
