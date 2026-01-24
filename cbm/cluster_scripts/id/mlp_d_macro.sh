eb07f2d9bbc8160f74bf07633bff1db278be88d3
simonbing <bings@student.ethz.ch>
2026-01-24 14:13:13 +0100

    cluster script

--- cbm/cluster_scripts/id/mlp_d_macro.sh
+++ cbm/cluster_scripts/id/mlp_d_macro.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
 sbatch ../cluster_gpu.sh python ../../identifiability_experiment.py \
---seed 42 --x d_macro --x_values 3 --n_samples 50000 --n_seeds 1 --estimation_mode mlp \
+--seed 0 --x d_macro --x_values 3 --n_samples 50000 --n_seeds 5 --estimation_mode mlp \
 --results_root /work/bd1083/b382081/projects/CausalBottleneckModels/results_2layer_swish_deep_mlp_d_macro_3_extra
